60f0361a05d02f28481b41205f946590646a3c05
“岳小木” <ypj_whut@163.com>
2015-12-30 18:19:56 +0800

    12/30 新建项目

---
+++ 课程设计-3D导航/课程设计-3D导航/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  课程设计-3D导航
+//
+//  Created by Etouch on 15/12/30.
+//  Copyright © 2015年 iceB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
